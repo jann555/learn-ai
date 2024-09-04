745c8f051619986c2fb9edc8c65eba46d0e8e370
Jannick Ocampo <focampo3@gatech.edu>
2024-09-03 20:20:42 -0400

    working using wrong model

--- src/adaptation_foundation_models/light_weight_fine_tuning.py
+++ src/adaptation_foundation_models/light_weight_fine_tuning.py
@@ -2,33 +2,38 @@
 # TODO: In the lines below, load your chosen pre-trained Hugging Face model and evaluate its performance
 #  prior to fine-tuning. This step includes loading an appropriate tokenizer and dataset.
 import numpy as np
+from torch import float16
 from datasets import load_dataset
-from transformers import (AutoModelForSequenceClassification, AutoTokenizer,
-                          Trainer, TrainingArguments, DataCollatorWithPadding, GPT2Tokenizer, AutoModelForCausalLM)
-from peft import LoraConfig, get_peft_model, AutoPeftModelForCausalLM
+from transformers import (AutoTokenizer, Trainer, TrainingArguments, DataCollatorWithPadding, AutoModelForCausalLM,
+                          AutoModelForSequenceClassification)
+
+from peft import LoraConfig, get_peft_model, TaskType
 
 dataset = load_dataset('google-research-datasets/poem_sentiment', split="train").train_test_split(
-    test_size=0.2, shuffle=True, seed=23
+    test_size=0.2, shuffle=True, seed=15
 )
-model_name = "gpt2"
+model_name = "bert-base-uncased"
 
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 tokenized_dataset = {}
 splits = ["train", "test"]
-print(dataset["train"][0])
+
 for split in splits:
     tokenized_dataset[split] = dataset[split].map(
-        lambda x: tokenizer(x["verse_text"], truncation=True, padding="max_length", max_length=8), batched=True
+        lambda x: tokenizer(x["verse_text"], truncation=True, padding=True), batched=True
     )
 
 print("tokenized_dataset", tokenized_dataset["train"])
+print("tokenized_dataset", tokenized_dataset["test"])
 
-model = AutoModelForCausalLM.from_pretrained(
+model = AutoModelForSequenceClassification.from_pretrained(
     model_name,
     num_labels=4,
     id2label={0: "negative", 1: "positive", 2: "no impact", 3: "mixed"},
-    label2id={"negative": 0, "positive": 1, "no impact": 2, "mixed": 3}
+    label2id={"negative": 0, "positive": 1, "no impact": 2, "mixed": 3},
 )
+
+print(model)
 
 # Performing Parameter-Efficient Fine-Tuning
 # TODO: In the lines below, create a PEFT model from your loaded model, run a training loop, and save the
@@ -36,11 +41,10 @@ model = AutoModelForCausalLM.from_pretrained(
 
 
 config = LoraConfig(
-    r=8,
-    lora_alpha=8,
-    target_modules="all-linear",
-    bias="none",
-    lora_dropout=0.1
+    r=4,
+    lora_alpha=16,
+    lora_dropout=0.1,
+    target_modules=["query", "value"]
 )
 lora_model = get_peft_model(model, config)
 lora_model.print_trainable_parameters()
@@ -48,12 +52,12 @@ lora_model.print_trainable_parameters()
 
 def compute_metrics(eval_pred):
     predictions, labels = eval_pred
-    predictions = np.argmax(predictions, axis=2)
+    predictions = np.argmax(predictions, axis=1)
     return {"accuracy": (predictions == labels).mean()}
 
 
 trainer = Trainer(
-    model=lora_model,
+    model=model,
     args=TrainingArguments(
         output_dir="./lora_results",
         learning_rate=2e-5,
@@ -62,7 +66,8 @@ trainer = Trainer(
         eval_strategy="epoch",
         save_strategy="epoch",
         num_train_epochs=1,
-        weight_decay=0.01
+        weight_decay=0.01,
+        load_best_model_at_end=True,
     ),
     train_dataset=tokenized_dataset["train"],
     eval_dataset=tokenized_dataset["test"],
@@ -78,9 +83,8 @@ lora_model.save_pretrained("bert-lora")
 
 # Inference with PEFT
 # Loading a Saved PEFT Model
-lora_model = AutoModelForCausalLM.from_pretrained("bert-lora", ignore_mismatched_sizes=True)
+lora_model = AutoModelForSequenceClassification.from_pretrained("bert-lora", ignore_mismatched_sizes=True)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 inputs = tokenizer("and that is why, the lonesome day,", return_tensors="pt")
 outputs = model.generate(input_ids=inputs["input_ids"], max_new_tokens=8)
 print(tokenizer.batch_decode(outputs))
-
